9f73be77ffec9433be222455f77ad7c86bb06a87
gkdidms <gkdidms@naver.com>
2023-07-04 18:11:56 +0900

    ✨ 2023/07/04

---
+++ src/3004.kt
@@ -0,0 +1,19 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+fun main() {
+    val buffer = BufferedReader(InputStreamReader(System.`in`))
+    val n = buffer.readLine()!!.toInt()
+
+    var cut = 1
+    var sum = 1
+    for (i in 1 .. n) {
+
+        if (i % 2 == 0) {
+            cut += 1
+        }
+        sum += cut
+    }
+    print(sum)
+}  
