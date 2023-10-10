d7cb1a5ea8a438c467122f4316e350d3fa170aa6
gkdidms <gkdidms3320@gmail.com>
2023-10-10 21:25:56 +0900

    원형 리스트 예제

--- DataStructure/DataStructure/원형 연결 리스트.cpp
+++ DataStructure/DataStructure/원형 연결 리스트.cpp
@@ -33,12 +33,90 @@ int main()
 }
 
 LinkedList_h* CreateLinkedList_h() {
-
+	LinkedList_h* cl = new LinkedList_h;
+	cl->head = nullptr;
+	return cl;
 }
-void printList(LinkedList_h* cl);
-void insertFirstNode(LinkedList_h* cl, int _iNum);
-void insertMiddleNode(LinkedList_h* cl, Node* pre, int _iNum);
-void deleteNode(LinkedList_h* cl, Node* old);
-Node* searchNode(LinkedList_h* cl, int _iNum);
 
-//아 하기싫어 죽겠넴 ㅋㅋ
+void printList(LinkedList_h* cl) {
+	Node* p;
+	
+	p = cl->head;
+
+	while (p != cl->head) {
+		cout << p->data << endl;
+		p = p->link;
+		if (p != cl->head) cout << ",";
+	}
+	cout << endl << endl;
+}
+
+//첫번째 노드 삽입 연산
+void insertFirstNode(LinkedList_h* cl, int _iNum) {
+	Node* newNode, * Temp;
+	newNode = new Node;
+	newNode->data = _iNum;
+
+	if (cl->head == nullptr) { // 원형 연결 리스트가 공백일 경우
+		cl->head = newNode;
+		newNode->link = newNode;
+	}
+	else {
+		Temp = cl->head;
+		while (Temp->link != cl->head)
+			Temp = Temp->link;
+		newNode->link = Temp->link;
+		Temp->link = newNode;
+		cl->head = newNode;
+	}
+}
+
+//pre 뒤에 노드를 삽입하는 연산
+void insertMiddleNode(LinkedList_h* cl, Node* pre, int _iNum) {
+	Node* newNode = new Node;
+	newNode->data = _iNum;
+
+	if (cl == NULL) {
+		cl->head = newNode;
+		newNode->link = newNode;
+	}
+	else {
+		newNode->link = pre->link;
+		pre->link = newNode;
+	}
+}
+
+//원형 연결 리스트의 pre 뒤에 있는 노드 old를 삭제하는 연산
+void deleteNode(LinkedList_h* cl, Node* old) {
+	Node* pre;
+	if (cl->head == nullptr) return;
+	if (cl->head->link == nullptr) { // 리스트에 노드가 한개만 있는 경우.
+		delete cl->head;
+		cl->head = nullptr;
+		return;
+	}
+	else if (old == nullptr) return; // 삭제할 노드가 없는 경우 삭제 연산자 중단
+	else {
+		pre = cl->head;
+		while (pre->link != old)
+			pre = pre->link; // 선행자 노드를 포인터 pre를 이용해 찾음.
+
+		pre->link = old->link;
+		if (old == cl->head)
+			cl->head = old->link; 
+		delete old;
+	}
+}
+
+Node* searchNode(LinkedList_h* cl, int _iNum) {
+	Node* pTemp;
+	pTemp = cl->head;
+
+	while (pTemp != nullptr) {
+		if (pTemp->data == _iNum) return pTemp;
+		else pTemp = pTemp->link;
+	}
+
+	return pTemp;
+}
+
